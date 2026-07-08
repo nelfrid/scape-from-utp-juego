1e568981a8c4f766ea9cc7fb36c89cdfccf4a0de
Raichu7801 <oswelcardona94@gmail.com>
2026-07-08 01:07:05 -0500

    Implementación de Prefabs de obstáculos

--- Assets/Scripts/SaludJugador.cs
+++ Assets/Scripts/SaludJugador.cs
@@ -5,9 +5,15 @@ public class SaludJugador : MonoBehaviour
     public int vidaMaxima = 100;
     private int vidaActual;
 
+    // 1. Declaramos la variable para el Animator del personaje
+    private Animator anim;
+
     void Start()
     {
         vidaActual = vidaMaxima;
+
+        // 2. Buscamos y guardamos el componente Animator al iniciar el juego
+        anim = GetComponent<Animator>();
     }
 
     public void RecibirDaño(int cantidad)
@@ -24,6 +30,19 @@ public class SaludJugador : MonoBehaviour
     void Morir()
     {
         Debug.Log("El jugador ha muerto.");
-        // Aquí puedes reiniciar el nivel o activar una animación de derrota
+
+        // 3. Activamos el Trigger en el Animator para que empiece la animación
+        // Asegúrate de cambiar "Muerte" por el nombre exacto que le pusieron al parámetro en Unity
+        if (anim != null)
+        {
+            anim.SetTrigger("Muerte");
+        }
+
+        // 4. Detenemos los puntos llamando al script de ScorePorTiempo que está en el GameManager
+        ScorePorTiempo scoreTiempo = FindAnyObjectByType<ScorePorTiempo>();
+        if (scoreTiempo != null)
+        {
+            scoreTiempo.DetenerPuntaje();
+        }
     }
 }
